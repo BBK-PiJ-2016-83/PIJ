96b7bcc396597d32f6f2e20c056d653668be24ee
dyanev1983 <dyanev1983@yahoo.com>
2019-02-02 06:01:03 +0000

    bew

--- Day.6/Hospital/Test.java
+++ Day.6/Hospital/Test.java
@@ -3,6 +3,7 @@ public class Test{
 	public static void main(String... args){
 		System.out.println("GIT");
 		System.out.println("Another message");
+		System.out.println("Another message !!!");
 	}
 
 }
